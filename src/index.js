85b200bc0d200aee2e421427403eb376113cc40d
keith212005 <45622241+keith212005@users.noreply.github.com>
2020-11-28 19:08:29 +0530

    redux store setup with redux persist

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
+
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import {PersistGate} from 'redux-persist/integration/react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import {StackNavigator} from '@navigator';
+import {autoLogin} from '@reducers';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  // whitelist: ['autoLogin'],
+  // blacklist: ['autoLogin'],
+};
+
+const rootReducer = combineReducers({
+  autoLogin: autoLogin,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
 
 export default class App extends React.Component {
   render() {
-    return <StackNavigator />;
+    return (
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <StackNavigator />
+        </PersistGate>
+      </Provider>
+    );
   }
 }
